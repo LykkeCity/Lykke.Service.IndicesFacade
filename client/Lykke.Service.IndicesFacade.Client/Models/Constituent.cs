dfb1a73a2808041b04ae60d487f30b087d04b2fb
Alexey Zelenin <alexey@zelenin.org>
2019-01-09 16:32:53 +0300

    Added models

---
+++ client/Lykke.Service.IndicesFacade.Client/Models/Constituent.cs
@@ -0,0 +1,18 @@
+﻿namespace Lykke.Service.IndicesFacade.Client.Models
+{
+    /// <summary>
+    /// Constituent of the index
+    /// </summary>
+    public class Constituent
+    {
+        /// <summary>
+        /// Asset id
+        /// </summary>
+        public string AssetId { get; set; }
+
+        /// <summary>
+        /// Weight of the current constituent
+        /// </summary>
+        public string Weight { get; set; }
+    }
+}
---
+++ client/Lykke.Service.IndicesFacade.Client/Models/HistoryElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Lykke.Service.IndicesFacade.Client.Models
+{
+    /// <summary>
+    /// Index history element.
+    /// </summary>
+    public class HistoryElement
+    {
+        /// <summary>
+        /// Value of the index
+        /// </summary>
+        [JsonProperty(PropertyName = "v")]
+        public decimal Value { get; set; }
+
+        /// <summary>
+        /// Timestamp
+        /// </summary>
+        [JsonProperty(PropertyName = "dt")]
+        public DateTime Timestamp { get; set; }
+    }
+}
